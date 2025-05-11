165354ceaa9ecabd9c78f1bf7cea379176cb9efc
Amit Parekh <parekhamit04@gmail.com>
2025-05-11 20:50:50 +0530

    fix: hierarchical di

--- src/app/core/component.ts
+++ src/app/core/component.ts
@@ -6,23 +6,34 @@ export function Component(options: ComponentOptions) {
     target.ɵcmp = {
         selector: options.selector,
         providers: options.providers || [],
-        imports: options.imports || [],
         styles: options.styles || [],
         template: options.template || "",
     };
-    
+
     const customElementConstructor = class extends HTMLElement {
-        private componentInjector!: Injector;
+        private componentInjector!: Injector | null;
         private componentInstance: any;
-
-        constructor() {
-            super();
-            this.componentInjector = Injector.create(target.ɵcmp.providers, Injector.getCurrentInjector());
-            Injector.setCurrentInjector(this.componentInjector);
-            this.componentInstance = new target();
+        
+        getParentInjector(): Injector | null {
+            let parentNode = this.parentNode;
+            while (parentNode) {
+                if (parentNode instanceof HTMLElement && 'componentInjector' in parentNode)
+                    return parentNode.componentInjector as Injector;
+                parentNode = parentNode.parentNode;
+            }
+            return null;
         }
-
+        
         connectedCallback() {
+            const parentInjector = this.getParentInjector();   // Current injector must be of parent web component! NULL for root component
+            if (target.ɵcmp.providers.length) { // create injector only if providers are included
+                this.componentInjector = Injector.create(target.ɵcmp.providers, parentInjector);    // Create injector everytime webcomponent is instantiated
+                Injector.setCurrentInjector(this.componentInjector);
+            }
+            else if (parentInjector) {
+                Injector.setCurrentInjector(parentInjector);
+            }
+            this.componentInstance = new target();
             if (typeof this.componentInstance.onInit == "function") this.componentInstance.onInit();
             this.innerHTML = target.ɵcmp.template;
             if (typeof this.componentInstance.afterViewInit == "function") this.componentInstance.afterViewInit();
@@ -34,6 +45,8 @@ export function Component(options: ComponentOptions) {
 
         disconnectedCallback() {
             if (typeof this.componentInstance.onDestroy == "function") this.componentInstance.onDestroy();
+            this.componentInstance = null;
+            this.componentInjector = null;
         }
     }
 
--- src/main.ts
+++ src/main.ts
@@ -29,13 +29,33 @@ class HttpClientTest {
 }
 
 @Component({
+    selector: "app-button",
+    template: `<button type="button">Login</button>`
+})
+class ButtonComponent implements OnInit {
+    http = inject(HttpClient);
+
+    onInit(): void {
+        this.http.get("");
+    }
+}
+
+@Component({
     selector: "app-header",
-    template: `<p>Header component!</p>`,
+    template: `<p>Header component! <app-button></app-button></p>`,
     providers: [
         { provide: HttpClient, useClass: HttpClientTest }
     ]
 })
 class HeaderComponent implements OnInit {
+    onInit(): void {}
+}
+
+@Component({
+    selector: "app-left-nav",
+    template: `<p>LeftNav component!</p>`,
+})
+class LeftNavComponent implements OnInit {
     http = inject(HttpClient);
 
     onInit(): void {
@@ -49,9 +69,9 @@ class HeaderComponent implements OnInit {
         <app-header></app-header>
         <h1>Hello, World!</h1>
         <p>Welcome to your first web component!</p>
+        <app-left-nav></app-left-nav>
     `,
-    providers: [HttpClient, DataService],
-    imports: [HeaderComponent]
+    providers: [HttpClient, DataService]
 })
 class AppComponent implements OnInit {
     onInit(): void {}
